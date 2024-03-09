416c67eedf7864ac50e82cf9ead48b13f7c5412d
Nizar Bahajoub <n.bahajoub@gmail.com>
2024-03-09 19:31:46 +0000

    Task 1dvanced

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,15 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser() {
+  let result = {};
+
+  try {
+    const pic = await uploadPhoto();
+    const user = await createUser();
+    result = { pic, user };
+  } catch (err) {
+    result = { photo: null, user: null };
+  }
+
+  return result;
+}

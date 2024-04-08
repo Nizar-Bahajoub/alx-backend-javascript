e91246b1fb87a8edd3df06299e80d93caa3da497
Nizar-Bahajoub <116908630+Nizar-Bahajoub@users.noreply.github.com>
2024-04-08 22:43:53 +0000

    Task 3

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,9 +1,9 @@
 export default class Currency {
     constructor(code, name) {
-        if (typeof name != 'string'){
+        if (typeof name !== 'string'){
             throw new TypeError('Name must be a string');
         }
-        if (typeof code != 'string') {
+        if (typeof code !== 'string') {
             throw new TypeError('Code must be a string')
         }
         this._name = name;

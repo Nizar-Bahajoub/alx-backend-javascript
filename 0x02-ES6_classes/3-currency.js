5fd2aa943190929e17f1f91684f0eff61eaaf9a5
Nizar-Bahajoub <116908630+Nizar-Bahajoub@users.noreply.github.com>
2024-04-08 22:52:49 +0000

    Task 3

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,43 +1,43 @@
 export default class Currency {
-    constructor(code, name) {
-        if (typeof name !== 'string'){
-            throw new TypeError('Name must be a string');
-        }
-        if (typeof code !== 'string') {
-            throw new TypeError('Code must be a string')
-        }
-        this._name = name;
-        this._code = code;
+  constructor(code, name) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
     }
+    if (typeof code !== 'string') {
+      throw new TypeError('Code must be a string')
+    }
+    this._name = name;
+    this._code = code;
+  }
 
-    /**
-     * @param {String} name
-     */
-    set name(name) {
-        if (typeof name !== 'string') {
-            throw new TypeError('Name must be a string');
-        }
-        this._name = name;
+  /**
+   * @param {String} name
+ */
+  set name(name) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
     }
+    this._name = name;
+  }
 
-    get name(){
-        return this._name;
-    }
+  get name() {
+    return this._name;
+  }
 
-    /**
-     * @param {String} code
-     */
-    set code(code){
-        if (typeof code != 'string') {
-            throw new TypeError('Code must be a string')
-        }
+  /**
+   * @param {String} code
+   */
+  set code(code) {
+    if (typeof code != 'string') {
+      throw new TypeError('Code must be a string')
     }
+  }
 
-    get code(){
-        return this._code;
-    }
+  get code() {
+    return this._code;
+  }
 
-    displayFullCurrency() {
-        return `${this._name} (${this._code})`;
-    }
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
 }

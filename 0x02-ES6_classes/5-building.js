38d182837d233754a6dba777a782087cabe98a4a
Nizar-Bahajoub <116908630+Nizar-Bahajoub@users.noreply.github.com>
2024-04-09 00:47:00 +0000

    Task 6

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,6 +1,6 @@
-export default class Buildings {
+export default class Building {
   constructor(sqft) {
-    if (this.constructor !== Buildings) {
+    if (this.constructor !== Building) {
       if (typeof this.evacuationWarningMessage !== 'function') {
         throw new Error('Class extending Building must override evacuationWarningMessage');
       }

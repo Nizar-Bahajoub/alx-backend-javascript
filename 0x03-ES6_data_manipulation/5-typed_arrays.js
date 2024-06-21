ca3e27a0a389f6d130baad047893ae175d938164
Nizar Bahajoub <n.bahajoub@gmail.com>
2024-06-21 08:23:17 +0000

    Task 5

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,4 +1,4 @@
-export default createInt8TypedArray(lenght, position, value) {
+export default function createInt8TypedArray(length, position, value) {
   if (position >= length) throw Error('Position outside range');
   const buffer = new ArrayBuffer(length);
   const newView = new DataView(buffer, 0, length);

92e07ff5dcb4292237c410059599ae9491eea80f
Nizar <n.bahajoub@gmail.com>
2024-12-10 10:17:09 +0000

    Tasks 5 to 7 Done

---
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -0,0 +1,8 @@
+//Payment Token
+const getPaymentTokenFromAPI = (success) => new Promise((resolve, _reject) => {
+	if (success) {
+		resolve({data: 'Successful response from the API'});
+	}
+});
+
+module.exports = getPaymentTokenFromAPI;

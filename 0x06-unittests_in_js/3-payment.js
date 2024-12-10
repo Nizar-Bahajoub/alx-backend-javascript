7edcc3935a7aea12f9691ed467b50053386dea40
Nizar <n.bahajoub@gmail.com>
2024-12-10 09:48:46 +0000

    Tasks From 2 to 4 Done

---
+++ 0x06-unittests_in_js/3-payment.js
@@ -0,0 +1,9 @@
+//Payment function
+const Utils = require('./utils');
+
+function sendPaymentRequestToApi(totalAmount, totalShipping) {
+	const totalCost = Utils.calculateNumber('SUM', totalAmount, totalShipping);
+	console.log(`The total is: ${totalCost}`);
+};
+
+module.exports = sendPaymentRequestToApi;
---
+++ 0x06-unittests_in_js/4-payment.js
@@ -0,0 +1,9 @@
+//Payment function
+const Utils = require('./utils');
+
+const sendPaymentRequestToApi = (totalAmount, totalShipping) => {
+	const totalCost = Utils.calculateNumber('SUM', totalAmount, totalShipping);
+	console.log(`The total is: ${totalCost}`);
+};
+
+module.exports = sendPaymentRequestToApi;
